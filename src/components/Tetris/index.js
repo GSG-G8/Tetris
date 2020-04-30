77b587ec976c995e9ea06a80f62332851ef0f137
rehabas <reremo45@gmail.com>
2020-04-30 22:47:35 +0300

    use player rotate in tetries relates #23

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -15,7 +15,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, resetPlayer, updatePlayerPos] = usePlayer();
+  const [player, resetPlayer, updatePlayerPos, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   const startGame = () => {
@@ -43,6 +43,8 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
