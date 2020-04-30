14803b65e556478bf3910b57bbac9be472982763
rehabas <reremo45@gmail.com>
2020-04-30 18:11:21 +0300

    clear the stage from previous render relates #19

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../utils/createStage';
 
-const useStage = () => {
+const useStage = (player, resetPLayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // Clear the stage from previous render.
+      const newStage = prevStage.map((row) =>
+        row.map((oneCell) => (oneCell[1] === 'clear' ? [0, 'clear'] : oneCell))
+      );
+    };
+    setStage((prev) => updateStage(prev));
+  }, []);
 
   return [stage, setStage];
 };
