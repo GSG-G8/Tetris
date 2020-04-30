45f7436f138a936de923a525653c85d80b72a25d
rehabas <reremo45@gmail.com>
2020-04-30 18:31:08 +0300

    make the stage blank at first render relates #19

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-import { randomTetromino } from '../utils/tetrominos';
+import { TETROMINOUS, randomTetromino } from '../utils/tetrominos';
 
 const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOUS[0].shape,
     collided: false,
   });
   return [player];
