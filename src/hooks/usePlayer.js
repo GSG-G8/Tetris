ba0425788c6a7277614fe15367579e160d393871
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-04-30 17:16:35 +0300

    make two function, update Player position and reset Player state relates #18

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import { randomTetromino } from '../utils/tetrominos';
+import { STAGE_WIDTH } from '../utils/createStage';
 
 const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -8,7 +9,24 @@ const usePlayer = () => {
     tetromino: randomTetromino().shape,
     collided: false,
   });
-  return [player];
+
+  const updatePlayerPos = ({ x, y, collided }) => {
+    setPlayer((previous) => ({
+      ...previous,
+      pos: { x: (previous.pos.x += x), y: (previous.pos.y += y) },
+      collided,
+    }));
+  };
+
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 + 1, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false,
+    });
+  }, []);
+
+  return [player, setPlayer, updatePlayerPos, resetPlayer];
 };
 
 export default usePlayer;
